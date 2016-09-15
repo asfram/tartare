654e2bec696cb2f69fc5b51ada07a36b2ff64363
Franck Assi <franck.assi@canaltp.fr>
2016-09-15 11:46:13 +0200

    Use coverages api to get technical conf input_dir for a coverage...adding tests

--- tests/integration/mongo/rest_api_test.py
+++ tests/integration/mongo/rest_api_test.py
@@ -40,7 +40,7 @@ from tests.utils import to_json
 def coverage_jdr(app):
     coverage_jdr = app.post('/coverages',
                 headers={'Content-Type': 'application/json'},
-               data='{"id": "jdr", "name": "name of the coverage jdr", "input_dir": "/srv/tartare/id_test/input"}')
+               data='{"id": "jdr", "name": "name of the coverage jdr"}')
     return coverage_jdr
 
 
@@ -50,7 +50,7 @@ def test_post_grid_calendar_returns_success_status(app, coverage_jdr):
     files = {'file': (open(path, 'rb'), 'export_calendars.zip')}
     raw = app.post('/coverages/jdr/grid_calendar', data=files)
     r = to_json(raw)
-    input_dir = coverage_jdr.technical_conf.input_dir
+    input_dir = coverage_jdr['technical_conf']['input_dir']
     assert input_dir == '/srv/tartare/id_test/input'
     assert raw.status_code == 200
     assert r.get('message') == 'OK'
