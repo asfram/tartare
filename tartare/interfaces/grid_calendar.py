5434349c74e20177f3d8057ba727a3b0bb4e21ce
Franck Assi <franck.assi@canaltp.fr>
2016-09-15 12:17:13 +0200

    Use coverages api to get technical conf input_dir for a coverage...adding tests

--- tartare/interfaces/grid_calendar.py
+++ tartare/interfaces/grid_calendar.py
@@ -38,7 +38,6 @@ from flask_restful import Resource
 from tartare import app
 from tartare.core import models
 import shutil
-import json
 
 GRID_CALENDARS = "grid_calendars.txt"
 GRID_CALENDARS_HEADER = {'id', 'name', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday',
@@ -104,9 +103,7 @@ class GridCalendar(Resource):
             return {'message': 'non-compliant file(s) : {}'.format(''.join(invalid_files))}, 400
 
         # backup content
-        import pprint
-        pprint.pprint(coverage)
-        input_dir = json.loads(coverage.data.decode('utf-8'))['coverage']['technical_conf']['input_dir']
+        input_dir = coverage.technical_conf.input_dir
         if not os.path.exists(input_dir):
             os.makedirs(input_dir)
         content.stream.seek(0)
